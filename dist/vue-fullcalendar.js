50b52cf2002fc72830496b0f18a32dc511a4b359
WangXiaoxuan <sunny@xiaoman.cn>
2016-12-16 16:42:46 +0800

    issue fix

--- dist/vue-fullcalendar.js
+++ dist/vue-fullcalendar.js
@@ -1,5 +1,5 @@
 /*!
- * vue-fullcalendar v1.0.6
+ * vue-fullcalendar v1.0.7
  * (c) 2016 Sunny Wang <sunnywang0104@163.com> 
  * @license MIT
  */
@@ -597,7 +597,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	  },
 	  zh: {
 	    weekNames: ['周日', '周一', '周二', '周三', '周四', '周五', '周六'],
-	    monthNames: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '11月', '12月'],
+	    monthNames: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
 	    titleFormat: 'yyyy年MM月'
 	  }
 	};
--- src/dataMap/langSets.js
+++ src/dataMap/langSets.js
@@ -1,12 +1,12 @@
 export default {
   en : {
-    weekNames :  ['Sun', 'Mon','Tue','Wed','Thu','Fri','Sat'],
-    monthNames : ['January','February','March','April','May','June','July','August','September','October','November','December'],
+    weekNames   : ['Sun', 'Mon','Tue','Wed','Thu','Fri','Sat'],
+    monthNames  : ['January','February','March','April','May','June','July','August','September','October','November','December'],
     titleFormat : 'MMMM yyyy'
   },
   zh : {
-    weekNames : ['周日', '周一','周二','周三','周四','周五','周六'],
-    monthNames : ['1月','2月','3月','4月','5月','6月','7月','8月','9月','11月','12月'],
+    weekNames   : ['周日', '周一','周二','周三','周四','周五','周六'],
+    monthNames  : ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
     titleFormat : 'yyyy年MM月'
   }
 }
